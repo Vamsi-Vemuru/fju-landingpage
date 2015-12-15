7a3797396eb81fa98579aeeb1c208fc9aab03587
sirimala <sirimala.sreenath@gmail.com>
2015-12-15 16:55:07 +0530

    design change for tab-bar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
 (function(){
 	var app =  angular.module("fju-landingpage", []);
-	app.controller("landing-page-controller", function($scope){
-		$scope.message = "hi";
-	});
+	app.controller("landing-page-controller", ['$scope', function($scope){
+		$scope.overview = overview();
+		$scope.eligibility = eligibility();
+		$scope.courses = courses();
+	}]);
+
+
+	app.controller('TabController', ['$scope', function () {
+        this.tab = 1;
+        this.setTab = function (tabId) {
+            this.tab = tabId;
+        };
+
+        this.isSet = function (tabId) {
+            return this.tab === tabId;
+        };
+    }]);
 })();
+
+function overview(){
+	return "course overview...";
+}
+
+function eligibility(){
+	return "your  eligibility...";
+}
+
+function courses(){
+	return "your courses...";
+}
